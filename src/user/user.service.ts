3d4649a74adda2d2de4613bd47dd2339896eab77
cyf0rk <marijanbatarilo.dev@gmail.com>
2025-04-28 18:57:36 +0200

    fix: change bcrypt to bcryptjs

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../database/prisma.service';
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 import { User } from 'db';
 
 type UserResponse = Omit<User, 'password'>;
