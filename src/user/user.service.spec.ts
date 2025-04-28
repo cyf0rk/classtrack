0f54037a1b7c5db2c8cf58d4a63dc03ecdd04f1c
cyf0rk <marijanbatarilo.dev@gmail.com>
2025-04-28 19:06:58 +0200

    test: change bcrypt package to bcryptjs

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
 import { PrismaService } from '../database/prisma.service';
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 
 describe('UserService', () => {
   let service: UserService;
