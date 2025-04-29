d87a55e33d4a368f12545e24e31da7d8d82522e0
cyf0rk <marijanbatarilo.dev@gmail.com>
2025-04-29 17:36:26 +0200

    refactor: change validate response type

--- src/auth/local.strategy.ts
+++ src/auth/local.strategy.ts
@@ -2,6 +2,7 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-local';
 import { UserService } from '../user/user.service';
+import type { UserResponse } from '../user/types';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
@@ -9,7 +10,7 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
     super({ usernameField: 'email' }); // Use email instead of username
   }
 
-  async validate(email: string, password: string): Promise<any> {
+  async validate(email: string, password: string): Promise<UserResponse> {
     const user = await this.userService.validateUser(email, password);
     if (!user) {
       throw new UnauthorizedException('Invalid credentials');
