682a37ab01725a60adc838a5a1d00142f296f366
cyf0rk <marijanbatarilo.dev@gmail.com>
2025-04-29 17:35:08 +0200

    feat: add CreateUserDto for user registration

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,20 +1,17 @@
 import { Controller, Post, Body, Request, UseGuards } from '@nestjs/common';
-import { Role } from 'db';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './local-auth.guard';
+import { CreateUserDto } from '../user/dto/create-user.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('register')
-  async register(
-    @Body() body: { email: string; password: string; role?: Role },
-  ) {
+  async register(@Body() createUserDto: CreateUserDto) {
     return this.authService.register(
-      body.email,
-      body.password,
-      body.role || Role.USER,
+      createUserDto.email,
+      createUserDto.password,
     );
   }
 
