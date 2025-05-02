45311277ca45facd8776a4bdfcfc72aaede05699
cyf0rk <marijanbatarilo.dev@gmail.com>
2025-05-02 09:06:00 +0200

    feat: add config module to app

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { AuthModule } from './auth/auth.module';
 import { RolesGuard } from './auth/roles.guard';
+import { AppConfigModule } from './config/config.module';
 
 @Module({
-  imports: [DatabaseModule, AuthModule],
+  imports: [AppConfigModule, DatabaseModule, AuthModule],
   controllers: [AppController],
   providers: [
     AppService,
