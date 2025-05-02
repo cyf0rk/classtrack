3b3176cf1d8fcd340ed31c9373677a9aefb4b95a
Marijan Batarilo <7797198+cyf0rk@users.noreply.github.com>
2025-05-02 09:53:09 +0200

    Feature/improve error handling (#7)
    * feat: add @nestjs/config package
    
    * feat: create config module
    
    * feat: add config module to app
    
    * feat: create http exception filter
    
    * feat: apply global http exception filter
    
    * feat: enable validation pipes

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalFilters(new HttpExceptionFilter(app.get(ConfigService)));
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
