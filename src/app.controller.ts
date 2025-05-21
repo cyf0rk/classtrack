4c27a572780fc9c9a29f4a592a0b0cefe57e0192
Marijan Batarilo <7797198+cyf0rk@users.noreply.github.com>
2025-05-21 14:06:49 +0200

    refactor: move guards and decorators to common folder (#9)

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,9 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { Role } from 'db';
 import { AppService } from './app.service';
-import { JwtAuthGuard } from './auth/jwt-auth.guard';
-import { Roles } from './auth/roles.decorator';
-import { RolesGuard } from './auth/roles.guard';
+import { JwtAuthGuard } from './common/guards/jwt-auth.guard';
+import { Roles } from './common/decorators/roles.decorator';
+import { RolesGuard } from './common/guards/roles.guard';
 
 @Controller()
 export class AppController {
