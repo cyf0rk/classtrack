47d7870b2c15190335ca6b9667e9ddcc5ba9cb36
Marijan Batarilo <7797198+cyf0rk@users.noreply.github.com>
2025-05-21 18:51:53 +0200

    Feature/add swagger (#11)
    * feat: add swagger package
    
    * feat: init swagger in app main

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
@@ -16,6 +17,17 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('ClassTrack API')
+    .setDescription(
+      'Classtrack is a RESTful API for managing sports classes, featuring user registration, role-based access, and full class scheduling and enrollment.',
+    )
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
