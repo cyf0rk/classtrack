fff7b3cd51a63f7b53427c3e45064ef1e5b55a1f
cyf0rk <marijanbatarilo.dev@gmail.com>
2025-05-21 18:42:24 +0200

    fix: exclude RolesGuard from guarding the whole app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,13 @@
 import { Module } from '@nestjs/common';
-import { APP_GUARD } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { AuthModule } from './auth/auth.module';
-import { RolesGuard } from './common/guards/roles.guard';
 import { AppConfigModule } from './config/config.module';
 
 @Module({
   imports: [AppConfigModule, DatabaseModule, AuthModule],
   controllers: [AppController],
-  providers: [
-    AppService,
-    {
-      provide: APP_GUARD,
-      useClass: RolesGuard,
-    },
-  ],
+  providers: [AppService],
 })
 export class AppModule {}
