84656122a3519bffc9bf1769b748f93dc23c9a72
cyf0rk <marijanbatarilo.dev@gmail.com>
2025-05-24 16:03:17 +0200

    feat: update register and login dtos

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,7 +1,7 @@
 import { IsEmail, IsString, MinLength, Matches } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
-export class CreateUserDto {
+export class LoginDto {
   @ApiProperty({
     example: 'user@example.com',
     description: 'User email address',
