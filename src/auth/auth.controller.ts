7e953e70dba3aa1ddb5ec2154961011580821eca
cyf0rk <marijanbatarilo.dev@gmail.com>
2025-05-24 16:04:42 +0200

    feat: add await to register and type for login request

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,14 +2,26 @@ import { Controller, Post, Body, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './local-auth.guard';
 import { CreateUserDto } from '../modules/user/dto/create-user.dto';
+import { ApiTags, ApiOperation, ApiBody, ApiResponse } from '@nestjs/swagger';
+import { LoginDto } from './dto/login.dto';
+import { UserResponse } from '../modules/user/types';
 
+interface RequestWithUser extends Request {
+  user: UserResponse;
+}
+
+@ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('register')
+  @ApiOperation({ summary: 'Register a new user' })
+  @ApiBody({ type: CreateUserDto })
+  @ApiResponse({ status: 201, description: 'User registered successfully' })
+  @ApiResponse({ status: 400, description: 'Invalid input data' })
   async register(@Body() createUserDto: CreateUserDto) {
-    return this.authService.register(
+    return await this.authService.register(
       createUserDto.email,
       createUserDto.password,
     );
@@ -17,7 +29,11 @@ export class AuthController {
 
   @UseGuards(LocalAuthGuard)
   @Post('login')
-  login(@Request() req) {
+  @ApiOperation({ summary: 'Login a user' })
+  @ApiBody({ type: LoginDto })
+  @ApiResponse({ status: 200, description: 'User logged in successfully' })
+  @ApiResponse({ status: 401, description: 'Invalid credentials' })
+  login(@Request() req: RequestWithUser) {
     return this.authService.login(req.user);
   }
 }
