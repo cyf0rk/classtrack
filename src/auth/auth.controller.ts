395fd7cf166fc07e957ae2733f17ec43735fe4a5
Marijan Batarilo <7797198+cyf0rk@users.noreply.github.com>
2025-05-24 16:11:52 +0200

    Feature/auth password validation (#19)
    * feat: update register and login dtos
    
    * feat: add await to register and type for login request
    
    * chore: add generic exection message for failed login
    
    * chore: change error for conflicting user
    
    * docs: add swagger 409 response for conflict

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,14 +2,30 @@ import { Controller, Post, Body, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './local-auth.guard';
 import { CreateUserDto } from '../modules/user/dto/create-user.dto';
+import { ApiTags, ApiOperation, ApiBody, ApiResponse } from '@nestjs/swagger';
+import { LoginDto } from './dto/login.dto';
+import { UserResponse } from '../modules/user/types';
 
+interface RequestWithUser extends Request {
+  user: UserResponse;
+}
+
+@ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('register')
+  @ApiOperation({ summary: 'Register a new user' })
+  @ApiBody({ type: CreateUserDto })
+  @ApiResponse({ status: 201, description: 'User registered successfully' })
+  @ApiResponse({ status: 400, description: 'Invalid input data' })
+  @ApiResponse({
+    status: 409,
+    description: 'User with this email already exists',
+  })
   async register(@Body() createUserDto: CreateUserDto) {
-    return this.authService.register(
+    return await this.authService.register(
       createUserDto.email,
       createUserDto.password,
     );
@@ -17,7 +33,11 @@ export class AuthController {
 
   @UseGuards(LocalAuthGuard)
   @Post('login')
-  login(@Request() req) {
+  @ApiOperation({ summary: 'Login a user' })
+  @ApiBody({ type: LoginDto })
+  @ApiResponse({ status: 200, description: 'User logged in successfully' })
+  @ApiResponse({ status: 401, description: 'Invalid credentials' })
+  login(@Request() req: RequestWithUser) {
     return this.authService.login(req.user);
   }
 }
