7c9e9b5e24785e382ca9db40cbf38d927bb623e2
cyf0rk <marijanbatarilo.dev@gmail.com>
2025-05-24 07:55:31 +0200

    feat: add class controller

--- src/modules/class/class.controller.ts
+++ src/modules/class/class.controller.ts
@@ -1,4 +1,53 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Patch,
+  Delete,
+  ParseIntPipe,
+} from '@nestjs/common';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
+import { ClassService } from './class.service';
+import { CreateClassDto } from './dto/create-class.dto';
+import { UpdateClassDto } from './dto/update-class.dto';
 
-@Controller('class')
-export class ClassController {}
+@ApiTags('Classes')
+@Controller('classes')
+export class ClassController {
+  constructor(private readonly service: ClassService) {}
+
+  @Post()
+  @ApiOperation({ summary: 'Create new class' })
+  create(@Body() dto: CreateClassDto) {
+    return this.service.create(dto);
+  }
+
+  @Get()
+  @ApiOperation({ summary: 'Get all classes' })
+  findAll() {
+    return this.service.findAll();
+  }
+
+  @Get(':id')
+  @ApiOperation({ summary: 'Get class by ID' })
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.service.findOne(id);
+  }
+
+  @Patch(':id')
+  @ApiOperation({ summary: 'Update class' })
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() dto: UpdateClassDto,
+  ) {
+    return this.service.update(id, dto);
+  }
+
+  @Delete(':id')
+  @ApiOperation({ summary: 'Delete class' })
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.service.remove(id);
+  }
+}
